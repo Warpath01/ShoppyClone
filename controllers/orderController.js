f68ad8ab9fd9524670200fabbfe2837e8614ce0e
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-04-24 10:16:59 +0800

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,16 +2,40 @@ const Product = require('../models/Product');
 const Order = require('../models/order');
 
 exports.showOrders = async (req, res) => {
-    const orders = await Order.find().populate('product');
-    res.render('orders', { orders });
+    try {
+        const orders = await Order.find().populate('product');
+        res.render('orders', { orders });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Failed to fetch orders.');
+    }
 };
 
 exports.createOrder = async (req, res) => {
-    await Order.create({ product: req.params.id });
-    res.redirect('/orders');
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).send('Product not found.');
+        }
+
+        await Order.create({ product: req.params.id });
+        res.redirect('/orders');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Failed to create order.');
+    }
 };
 
 exports.deleteOrder = async (req, res) => {
-    await Order.findByIdAndDelete(req.params.id);
-    res.redirect('/orders');
+    try {
+        const deleted = await Order.findByIdAndDelete(req.params.id);
+        if (!deleted) {
+            return res.status(404).send('Order not found.');
+        }
+
+        res.redirect('/orders');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Failed to delete order.');
+    }
 };
